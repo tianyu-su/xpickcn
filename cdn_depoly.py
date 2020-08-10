6f4d84ecd7348fab9d55b33fc1fd5de58df016d1
tianyu <tianyu.su@foxmail.com>
2020-08-10 14:20:23 +0800

    up

--- cdn_depoly.py
+++ cdn_depoly.py
@@ -1,19 +1,29 @@
 """
 替换静态文件, 使用CDN
 """
-CDN_VERSION = 1.0
+import argparse
 
 # setting file
 from pathlib import Path
 
-cnd_prifex = f'https://cdn.jsdelivr.net/gh/tianyu-su/xpickcn@{CDN_VERSION}/dist/static/'
-Path('backend/settings/prod.py').write_text(
-    Path('backend/settings/prod.py').read_text(encoding='utf-8').replace('###@@########CND URL PLACEHOLDER ####@@####',
-                                                                         f'STATIC_URL = "{cnd_prifex}"'),
-    encoding='utf-8')
 
-# replace static files
-files = ['dist/index.html', 'dist/static/about.html', 'dist/static/bindpage.html']
-for f in files:
-    Path(f).write_text(Path(f).read_text(encoding='utf-8').replace('../static/', cnd_prifex),
-                       encoding='utf-8')
+def main(cnd_version):
+    cnd_prefix = f'https://cdn.jsdelivr.net/gh/tianyu-su/xpickcn@{cnd_version}/dist/static/'
+    Path('backend/settings/prod.py').write_text(
+        Path('backend/settings/prod.py').read_text(encoding='utf-8').replace(
+            '###@@########CND URL PLACEHOLDER ####@@####',
+            f'STATIC_URL = "{cnd_prefix}"'),
+        encoding='utf-8')
+
+    # replace static files
+    files = ['dist/index.html', 'dist/static/about.html', 'dist/static/bindpage.html']
+    for f in files:
+        Path(f).write_text(Path(f).read_text(encoding='utf-8').replace('../static/', cnd_prefix),
+                           encoding='utf-8')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tag_version', type=str, help='release version for cdn')
+    args = parser.parse_args()
+    main(args.tag_version)
